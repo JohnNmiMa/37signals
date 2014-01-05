70687e27c48988b4965c165d3ec7b6e504ce3ae8
John <johnmarksjr@gmail.com>
2014-01-05 13:27:12 -0700

    Fixed the date class to show the real day of the week.

--- application.js
+++ application.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	$(".date").html(function() {
+		var today = (["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][(new Date()).getDay()]);
+		$(this).html("Happy " + today + ".");
+	})
 	$( ".basecard, .highcard, .campcard").hover(function() {
 		$( "center .first" ).toggle();
 	});
